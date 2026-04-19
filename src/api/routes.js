bcc0e3ce9cab206789b755b59bc8a49f2a2595ad
fedrimarcello <fedrimarcello@gmail.com>
2026-04-20 01:34:06 +0700

    Membuat endpoint untuk UAS Online

--- src/api/routes.js
+++ src/api/routes.js
@@ -9,6 +9,7 @@ const historiNilai = require('./components/HistoriNilai/HistoriNilai-route');
 const jadwalKuliah = require('./components/JadwalKuliah/JadwalKuliah-route');
 const uts = require('./components/uts/uts-route')
 const beasiswa = require('./components/DaftarBeasiswa/DaftarBeasiswa-route');
+const UasOnline = require('./components/UasOnline/UasOnline-route');
 
 module.exports = () => {
   const app = express.Router();
@@ -22,5 +23,6 @@ module.exports = () => {
   khs(app);
   uts(app);
   beasiswa(app);
+  UasOnline(app);
   return app;
 };
